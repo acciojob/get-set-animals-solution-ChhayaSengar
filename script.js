27469eb6e5da096a83337ca673539c68eaa476a2
Chhaya <117908142+ChhayaSengar@users.noreply.github.com>
2023-04-09 23:44:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,23 +2,25 @@ class Animal {
   constructor(species) {
     this._species = species;
   }
-
+// this is getter for species
   get species() {
     return this._species;
   }
-
+// this is method
   makeSound() {
     console.log("The " + this.species + " makes a sound");
   }
 }
-
+// it is subclass
 class Cat extends Animal {
+	// here i have add the method acc to question
   purr() {
     console.log("purr");
   }
 }
 
 class Dog extends Animal {
+	// here i have add the method  inside the subclass acc to question
   bark() {
     console.log("woof");
   }
